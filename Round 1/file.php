2a598ff3cb75be368034f4281c391d01d1adc2ef
atefAkl <atefakl80@gmail.com>
2025-09-03 20:50:50 +0300

    uploading files

--- Round 1/file.php
+++ Round 1/file.php
@@ -1,14 +1,4 @@
 <?php
-protected function schedule(Schedule $schedule)
-{
-    // Run a command every day at midnight
-    $schedule->command('reports:generate')->daily();
-
-    // Run a job every 5 minutes
-    $schedule->job(new \App\Jobs\SyncOrdersJob)->everyFiveMinutes();
-
-    // Run a closure every Monday at 8 AM
-    $schedule->call(function () {
-        \App\Models\User::inactive()->delete();
-    })->mondays()->at('08:00');
-}
+Route::get('/user/{type}', function ($type) {
+    return "User type: {$type}";
+})->whereIn('type', ['admin', 'customer', 'chef']);
